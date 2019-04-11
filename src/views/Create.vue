da3c109411bdfe037ee5aa5e1eddd49c495d006c
bruunoromero <bruunobromero@gmail.com>
2019-04-10 22:35:04 -0300

    makes buttons ok on both platforms

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -2,10 +2,23 @@
   <div>
     <ion-header>
       <ion-toolbar>
-        <ion-title>create</ion-title>
+        <ion-buttons slot="start">
+          <ion-back-button
+            @click="pop"
+            v-if="!isIos"
+            style="display: block"
+          ></ion-back-button>
+          <ion-button
+            v-if="isIos"
+            @click="pop"
+          >
+            Close
+          </ion-button>
+        </ion-buttons>
+        <ion-title>Create</ion-title>
         <ion-buttons slot="end">
-          <ion-button @click="pop">
-            <ion-icon slot="icon-only" name="close"></ion-icon>
+          <ion-button fill="clear">
+            Save
           </ion-button>
         </ion-buttons>
       </ion-toolbar>
@@ -15,15 +28,25 @@
       <ion-list>
         <ion-item>
           <ion-label position="floating">Nome</ion-label>
-          <ion-input @input="$v.name.$model = $event.target.value" :value="$v.name.$model"></ion-input>
+          <ion-input
+            :value="name"
+            @input="set('name', $event.target.value)"
+          ></ion-input>
         </ion-item>
         <ion-item>
           <ion-label position="floating">Valor</ion-label>
-          <ion-input type="number"></ion-input>
+          <ion-input
+            type="number"
+            :value="value"
+            @input="set('value', $event.target.value)"
+          ></ion-input>
         </ion-item>
         <ion-item>
           <ion-label position="floating">Local</ion-label>
-          <ion-input></ion-input>
+          <ion-input
+            :value="place"
+            @input="set('place', $event.target.value)"
+          ></ion-input>
         </ion-item>
       </ion-list>
     </ion-content>
@@ -31,22 +54,36 @@
 </template>
 
 <script>
-import { required } from "vuelidate/lib/validators";
+import { isPlatform } from "@ionic/core";
+import { required, numeric } from "vuelidate/lib/validators";
 
 export default {
   name: "create",
   data() {
     return {
-      name: ""
+      name: "",
+      value: "",
+      place: ""
     };
+  },
+  computed: {
+    isIos() {
+      return isPlatform(window, "ios");
+    }
   },
   methods: {
     pop() {
       this.$ionic.modalController.dismiss();
+    },
+    set(key, value) {
+      this[key] = value;
+      this.$v[key].$touch();
     }
   },
   validations: {
-    name: { required }
+    name: { required },
+    value: { required, numeric },
+    place: { required }
   }
 };
 </script>
